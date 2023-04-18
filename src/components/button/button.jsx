b1e943976c403c7206061a7cd2a7f4cdc69bbedc
kishan kumar <rajkishan7870@gmail.com>
2023-04-18 23:31:13 +0530

    updated work of day2 by kishan

--- src/components/button/button.jsx
+++ src/components/button/button.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import styles from './button.module.css'
 
-export default function Button({data, onClick, type}) {
+export default function Button({data, onClick, type, className}) {
   return (
     <div>
-        <button className = {styles.button} type ={type} onClick = {onClick}>{data}</button>
+        <button className = {className} type ={type} onClick = {onClick}>{data}</button>
     </div>
   )
 }
--- src/pages/LoginPage/LoginPage.jsx
+++ src/pages/LoginPage/LoginPage.jsx
@@ -1,41 +1,82 @@
-import React,{useRef} from 'react'
+import React,{useState, useRef, useEffect} from 'react'
 import Navbar from '../../components/Navbar'
 import Button from '../../components/button/button'
 import { getUsers } from '../../utils/localStorage';
 import styles from './Login.module.css'
+import { useNavigate } from 'react-router-dom';
 
 export default function LoginPage() {
   const userNameRef = useRef();
   const passwordRef = useRef();
 
+  const[userList,setUserList] = useState([{
+    usename : "",
+    password : ""
+  }])
+  
+  const nav = useNavigate();
+
+
+  useEffect(()=>{
+    const data = getUsers(); 
+    setUserList(data);
+   },[])
+  
+
   function handleSubmit(event){
     event.preventDefault();
-    const users = getUsers()
-    users.push({
-      username : userNameRef.current.value,
-      password : passwordRef.current.value
-    })
+    
+    if(userNameRef.current.value === "" || passwordRef.current.value === ""){
+      alert("FIll the form first")
+    }
 
-    localStorage.setItem("users", JSON.stringify(users))
+
+     const userObj = userList.find((obj)=> obj?.username === userNameRef.current.value && obj?.password === passwordRef.current.value)
+      
+    if(userObj !== undefined){
+      alert(`${userObj?.username} you are succ login`)
+      nav('/')
+    }else{
+      alert("please register first")
+    }
+  }
+
+  function handleSignUp(){
+    nav('/register')
   }
   return (
+
+   <div>
+      <Navbar/>
     <div className={styles.parentCont}>
-        <Navbar/>
-        <div>
-          <form  onSubmit={handleSubmit}>
-            <span>
-              <label>UserName : </label>
-              <input ref={userNameRef} type='text' placeholder='username'/>
-            </span>
 
-            <span>
-              <label>Password : </label>
-              <input ref={passwordRef} type='password' placeholder='password' />
-            </span>
+      <div className = {styles.login}>
 
-            <Button type="submit" data={"Sign In"} />
-          </form>
+            <div>
+                <h1>Login</h1>
+            </div>
+            <div>
+              <form  onSubmit={handleSubmit}>
+                <span>
+                  <label>UserName : </label>
+                  <input ref={userNameRef} type='text' placeholder='username'/>
+                </span>
+
+                <span>
+                  <label>Password : </label>
+                  <input ref={passwordRef} type='password' placeholder='password' />
+                </span>
+
+                <Button type="submit" data={"Sign In"} />
+              </form>
+            </div>
+            <div>
+              <span>Don't Have an account ?? <Button onClick={handleSignUp} data = {"Sign Up"}/></span>
+            </div>
+
         </div>
     </div>
+    </div>
+   
   )
 }
