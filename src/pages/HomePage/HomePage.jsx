e2a656bc2c9440643fc8f53f6b2ae68bd75b5706
Dk314053 <dk314053@gmail.com>
2023-06-05 02:48:14 +0530

    protected routing updated

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -5,8 +5,27 @@ import Training from '../TrainingPage/Training'
 import PricingSection from '../../pages/PricingSection/PricingSection'
 import Footer from '../../components/Footer/Footer'
 import styles from './HomePage.module.css'
+import { useEffect } from 'react'
+import { isUserLogInAtom } from '../Recoil'
+import { useRecoilValue } from 'recoil'
+import { useNavigate } from 'react-router-dom'
 
 export default function HomePage() {
+
+  const isUserLogedIn = useRecoilValue(isUserLogInAtom);
+  
+  const navigate=useNavigate()
+
+  useEffect(() => {
+  
+    if (!isUserLogedIn) {
+      navigate('/login');
+    }else{
+      navigate("/")
+    }
+  }, []);
+
+
   return (
     <div id='Home' className={styles.parentDiv}>
        
